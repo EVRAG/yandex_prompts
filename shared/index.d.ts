103547572e101adccfb778767875f2ea72efa6d2
EVR_AG <sanchezfr@mail.ru>
2025-12-07 17:14:43 +0100

    Clean slate: remove all files for fresh start

--- shared/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { GameConfig } from './types/gameConfig';
-export declare const gameConfig: GameConfig;
-export * from './types/gameConfig';
-//# sourceMappingURL=index.d.ts.map
--- shared/src/index.ts
+++
@@ -1,8 +0,0 @@
-import rawConfig from './gameConfig.json';
-import type { GameConfig } from './types/gameConfig';
-
-export const gameConfig = rawConfig as GameConfig;
-
-export * from './types/gameConfig';
-export * from './voteOptions';
-
